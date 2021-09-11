98142f62c00ee8f5d84987332b66b0f7a3dbc056
Carlos Jimenez <valuu1216@gmail.com>
2021-09-11 12:06:47 -0500

    Descuento del supermecado

--- Condicionales.py
+++ Condicionales.py
@@ -22,3 +22,20 @@ elif(numero_Camisa < 3):
     descuento_dos = total_camisa * 0.10
     total_pagar =  total_camisa - descuento_dos
     print (f'El valor total que debe pagar es de : ${total_pagar}')
+
+"""    
+2. En un supermercado se hace una promoción, mediante la cual el
+cliente obtiene un descuento dependiendo de un número que se
+escoge al azar. Si el número escogido es menor que 74 el descuento
+es del 15% sobre el total de la compra, si es mayor o igual a 74 el
+descuento es del 20%. Obtener cuanto dinero se le descuenta.
+"""
+
+valor_compra = float(input ('Digite el total de la compra : $  '))
+numero_escogido = int(input ('Digite el numero escogido : '))
+if (numero_escogido < 74):
+    descuento = valor_compra * 0.15
+    print (f'El descuento que se le hara es de  : ${descuento} ')
+elif (numero_escogido >= 74):
+    descuento_dos = valor_compra * 0.20
+    print (f'El descuento que se le hara es de : ${descuento_dos}')
